60858b3e63de6780fca5622768cd354d591fa0c3
tgentry300 <taylor.gentry300@gmail.com>
2019-03-17 23:49:16 -0400

    added url paths, jinja templates, and helper functions

--- recipeboxv1/recipeboxv1/helpers.py
+++ recipeboxv1/recipeboxv1/helpers.py
@@ -1,10 +1,23 @@
+from .models import Author, Recipe
+# from django.contrib.auth.models import User
+
+
 class AllRecipes:
-    pass
+    """Class get's all recipes from database"""
+    def get_all_recipes():
+        return Recipe.objects.all()
 
 
 class RecipeDetails:
-    pass
+    """Class get's recipe details"""
+    def get_recipe_by_id(recipe_id):
+        return Recipe.objects.get(id=recipe_id)
 
 
 class AuthorDetails:
-    pass
+    """Class get's author details"""
+    def get_author_details(author_name):
+        return Author.objects.get(name=author_name)
+
+    def get_all_recipes_by_author(author):
+        return Recipe.objects.filter(author=author)
--- recipeboxv1/recipeboxv1/views.py
+++ recipeboxv1/recipeboxv1/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-
-recipes = ['food', 'mor foods', 'pizza']
+from django.http import HttpResponse
+from .helpers import AllRecipes, RecipeDetails, AuthorDetails
 
 
 def all_recipes(request, **kwargs):
-    return render(request, 'recipes.html.j2', {'recipes': recipes})
+    recipe_list = AllRecipes.get_all_recipes()
+    return render(request, 'recipes.html.j2', {'recipes': recipe_list})
 
 
-def recipe_details(request, **kwargs):
-    return render(request, 'recipedetails.html.j2')
+def recipe_details(request, recipe_id):
+    recipe = RecipeDetails.get_recipe_by_id(recipe_id)
+    return HttpResponse(render(request, 'recipedetails.html.j2', {'recipe': recipe}))
 
 
-def author_details(request, **kwargs):
-    return render(request, 'authordetails.html.j2')
+def author_details(request, author_name):
+    author = AuthorDetails.get_author_details(author_name)
+    authors_recipes = AuthorDetails.get_all_recipes_by_author(author)
+    return HttpResponse(render(request, 'authordetails.html.j2', {'author': author, 'recipes': authors_recipes}))
